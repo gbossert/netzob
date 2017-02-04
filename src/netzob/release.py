2a461ac63f5509d58fedbe5557e15173572a6eeb
Georges Bossert <gbossert@miskin.fr>
2017-02-04 18:13:24 +0100

    update python package description

--- src/netzob/release.py
+++ src/netzob/release.py
@@ -53,7 +53,7 @@ copyright = "Copyright (C) 2011-2017 Georges Bossert and Frédéric Guihéry"
 #+---------------------------------------------------------------------------+
 #| Description of the application
 #+---------------------------------------------------------------------------+
-description = "Inferring communication protocols"
+description = "Reverse Engineering Communication protocols"
 
 #+---------------------------------------------------------------------------+
 #| Platforms on which the application can be executed
